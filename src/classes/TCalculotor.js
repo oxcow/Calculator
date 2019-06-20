f69022757a1d76d2aafefacaa19a9ae4069a98ab
William.Li <william.li@activenetwork.com>
2019-06-20 18:11:23 +0800

    Convert an infix expression to a postfix expression

--- src/classes/TCalculotor.js
+++ src/classes/TCalculotor.js
@@ -1,7 +1,5 @@
 // https://blog.csdn.net/luoweifu/article/details/10477447
 
-const midExpress = 'A+(B-C)/D)*E';
-
 const operators = Array.from('+-*/()');
 
 const L1 = Array.from("+-");
@@ -13,51 +11,70 @@ function isNum(str) {
 }
 
 function isGt(x, y) {
-    if (x === '#'){
-        console.log('x is #');
+    if (x === '#') {
+        //console.log('x is #');
         return false;
     }
-    if (y === '#') {
-        console.log('y is #')
+
+    if (x === ')') {
         return true;
     }
 
     if (L1.includes(x)) {
-
         if (L1.includes(y)) {
             return true;
         }
-
-        if (L3.includes(y) && y === ')') {
+        if (y === ')') {
             return true;
         }
     }
 
     if (L2.includes(x)) {
-        if (L2.includes(y)) {
+        if (L2.includes(y) || L1.includes(y)) {
             return true;
         }
-
-        if (L3.includes(y) && y === ')') {
+        if (y === ')') {
             return true;
         }
-    }
-
-    if (L3.includes(x) && x === ')') {
-        return true;
     }
 
     return false;
 }
 
+
+function stringExpressToArray(stringExpress) {
+
+    let k = [];
+
+    let t = [];
+
+    for (let e of Array.from(stringExpress)) {
+        if (isNum(e)) {
+            t.push(e);
+        } else {
+            if (t.length) {
+                k.push(Number(t.join('')));
+                t = [];
+            }
+            k.push(e);
+        }
+    }
+
+    if (t.length) {
+        k.push(Number(t.join('')));
+    }
+    // console.log(k)
+    return k;
+}
+
 function TCalculotor(midExpress) {
-    console.log('input: ', midExpress);
+    console.log('infix: ', midExpress);
 
 
     let S = ['#'];
     let L = [];
 
-    const targetExpress = Array.from(midExpress);
+    const targetExpress = stringExpressToArray(midExpress);
 
     for (let v of targetExpress) {
         if (isNum(v)) {
@@ -65,10 +82,11 @@ function TCalculotor(midExpress) {
             continue;
         }
         while (S.length) {
-            console.log(S, " >><< ", L);
+            //console.log(S, " >><< ", L);
             let x1 = S.pop();
             let x2 = v;
 
+            //console.log(x1, ', ', x2, ' isGt ', isGt(x1, x2));
             if (isGt(x1, x2)) {
                 L.push(x1);
             } else {
@@ -89,9 +107,67 @@ function TCalculotor(midExpress) {
             L.push(s);
         }
 
-        console.log('output: ', L);
+        // console.log('output: ', L);
     }
+
+    return L;
 }
 
-TCalculotor(midExpress);
+function cal(postfixExpression) {
+
+    const copyExpression = [...postfixExpression];
+
+    let len = copyExpression.length;
+
+    for (let i = 0; i < len; i++) {
+
+        const elment = copyExpression[i];
+
+        if (!isNum(elment)) {
+
+            const param1 = Number(copyExpression[i - 2]);
+            const param2 = Number(copyExpression[i - 1]);
+            let r;
+            switch (elment) {
+                case '+':
+                    r = param1 + param2;
+                    break;
+                case '-':
+                    r = param1 - param2;
+                    break;
+                case '*':
+                    r = param1 * param2;
+                    break;
+                case '/':
+                    r = param1 / param2;
+                    break;
+                default:
+            }
+            copyExpression.splice(i - 2, 3, r);
+            len -= 2;
+            i -= 2;
+        }
+    }
+    return copyExpression[0];
+}
+
+const midExpresses = [
+    '(((((((((8+1)-1)-1)-1)-1)-1)-1)-1)-1)', // 1
+    '(10+(3-1)/2)*3', // 33
+    '1+3*(0.5-10)/2', // -13.25
+    '1+3*(1-1)/2-2+3*7/3', // 6
+    '(1+((((1*2)/(3+1)+8)/(2-3))/1+3))+2*2'//-0.5
+];
+
+for (let midExpress of midExpresses) {
+    const postfixExpression = TCalculotor(midExpress);
+    console.log('postfix: ', postfixExpression);
+    console.log('result: ', cal(postfixExpression));
+}
+
+
+
+
+
+
 
